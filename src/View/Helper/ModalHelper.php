97bbe7c370e9a02122f5c0af48d07ce630a83f18
Alberto Rodriguez <arodu.dev@gmail.com>
2024-12-25 21:13:17 -0400

    add ModalHelper class for managing modal dialogs

---
+++ src/View/Helper/ModalHelper.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types=1);
+
+namespace BsUtils\View\Helper;
+
+use Cake\View\Helper;
+use Cake\View\View;
+
+/**
+ * Modal helper
+ */
+class ModalHelper extends Helper
+{
+    /**
+     * Default configuration.
+     *
+     * @var array<string, mixed>
+     */
+    protected array $_defaultConfig = [];
+}
