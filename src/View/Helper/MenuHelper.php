66a88f90bd63cd3d025c400a3fe22839dd617de3
Alberto Rodriguez <arodu.dev@gmail.com>
2024-12-27 14:00:01 -0400

    enhance MenuHelper with new item types and improved rendering for menu items

--- src/View/Helper/MenuHelper.php
+++ src/View/Helper/MenuHelper.php
@@ -16,6 +16,11 @@ class MenuHelper extends Helper
     use StringTemplateTrait;
     use InstanceConfigTrait;
 
+    const ITEM_TYPE_LINK = 'link';
+    const ITEM_TYPE_DIVIDER = 'divider';
+    const ITEM_TYPE_DISABLED = 'disabled';
+    const ITEM_TYPE_TITLE = 'title';
+
     /**
      * Default configuration.
      *
@@ -25,13 +30,16 @@ class MenuHelper extends Helper
         'nestClass' => 'dropdown',
         'activeClass' => 'active',
         'templates' => [
-            'menu' => '<ul class="menu">{{items}}</ul>',
-            'menuItem' => '<li class="{{class}}">{{text}}{{nest}}</li>',
-            'menuLink' => '<a href="{{url}}" class="">{{icon}}<span>{{text}}</span></a>',
+            'menu' => '<ul class="navbar-nav {{class}}">{{items}}</ul>',
+            'menuItem' => '<li class="nav-item {{class}}">{{text}}{{nest}}</li>',
+            'menuLink' => '<a href="{{url}}" class="nav-link {{class}}{{activeClass}}">{{icon}}{{text}}</a>',
+            'nestMenuLink' => '<a href="{{url}}" class="nav-link dropdown-toggle {{class}}{{activeClass}}" role="button" data-bs-toggle="dropdown" aria-expanded="false">{{icon}}{{text}}</a>',
             'nest' => '<ul class="dropdown-menu">{{items}}</ul>',
-            'nestItem' => '<li class="dropdown-item {{class}}">{{text}}{{nest}}</li>',
-            'nestLink' => '<a href="{{url}}" class="">{{icon}}{{text}}</a>',
+            'nestItem' => '<li>{{text}}{{nest}}</li>',
+            'nestLink' => '<a href="{{url}}" class="dropdown-item ">{{icon}}{{text}}</a>',
             'icon' => '<i class="{{icon}}"></i>',
+            'divider' => '<li><hr class="dropdown-divider"></li>',
+            'disabledItem' => '<li class="nav-item"><a class="nav-link disabled" aria-disabled="true">{{icon}}{{text}}</a></li>',
         ],
     ];
 
@@ -39,13 +47,27 @@ class MenuHelper extends Helper
 
     protected array $activeKeys = [];
 
+    /**
+     * @param array $menu
+     * @param array $options
+     * @return string
+     */
     public function render(array $menu, array $options = []): string
     {
-        $this->activeKeys = explode('.', $options['activeItem'] ?? '');
+        $this->activeItem($options['activeItem'] ?? '');
         return $this->formatTemplate('menu', [
-            'class' => $options['menu.class'] ?? $this->getConfig('menu.class') ?? 'menu',
+            'class' => $options['menu.class'] ?? $this->getConfig('menu.class'),
             'items' => $this->buildMenuItems($menu, $options),
         ]);
+    }
+
+    /**
+     * @param string $keys
+     * @return void
+     */
+    public function activeItem(string $keys): void
+    {
+        $this->activeKeys = explode('.', $keys);
     }
 
     /**
@@ -61,14 +83,35 @@ class MenuHelper extends Helper
         $isChild = $level > 0;
         $currentActiveKey = $this->activeKeys[$level] ?? null;
         foreach ($items as $key => $item) {
-            if (isset($item['type']) && $item['type'] === 'title' && !($isChild)) {
+            if (isset($item['hidden']) && $item['hidden'] === true) {
+                continue;
+            }
+
+            if (isset($item['hidden']) && is_callable($item['hidden']) && $item['hidden']()) {
+                continue;
+            }
+
+            if (isset($item['type']) && $item['type'] === self::ITEM_TYPE_TITLE && !($isChild)) {
                 $result .= $this->formatTemplate('menuItem', [
                     'text' => $item['title'],
                 ]);
                 continue;
             }
 
+            if (isset($item['type']) && $item['type'] === self::ITEM_TYPE_DIVIDER) {
+                $result .= $this->formatTemplate('divider', []);
+                continue;
+            }
+
+            if (isset($item['type']) && $item['type'] === self::ITEM_TYPE_DISABLED) {
+                $result .= $this->formatTemplate('disabledItem', [
+                    'text' => $item['title'],
+                ]);
+                continue;
+            }
+
             $template = $isChild ? 'nestLink' : 'menuLink';
+            $template = $item['children'] ?? null ? 'nestMenuLink' : $template;
             $link = $this->formatTemplate($template, [
                 'url' => $this->Url->build($item['url'] ?? '#'),
                 'icon' => !empty($item['icon']) ? $this->formatTemplate('icon', ['icon' => $item['icon']]) : '',
@@ -83,7 +126,6 @@ class MenuHelper extends Helper
             }
 
             $containerTemplate = $isChild ? 'nestItem' : 'menuItem';
-
             $result .= $this->formatTemplate($containerTemplate, [
                 'class' => $this->cssClass([
                     $item['class'] ?? null,
